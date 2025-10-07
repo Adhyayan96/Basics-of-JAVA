66bd1bb0dad54dd2a8fe09358fec6a60b2321dcb
Adhyayan96 <adhyayandas211@gmail.com>
2025-10-07 22:08:18 +0530

    optimized Dec. to Bin. conversion

--- Functioin/DectoBin.java
+++ Functioin/DectoBin.java
@@ -4,18 +4,13 @@ public class DectoBin {
     // Decimal to Binary Conversion
 
     public static void ByNum(int n){
-        int ans = 0;
+    StringBuilder sb = new StringBuilder();
         while (n>=1){
-            if(n==1){
-                ans = (10*ans)+(n%2);
-                break;
-            }
-            else{
-                ans = (10*ans)+(n%2);
+                int ans = n%2;
                 n = n/2;
-            }
+                sb.append(ans);            
         }
-        System.out.println("The binary number is: " + ans);
+        System.out.println(sb.reverse().toString());
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
